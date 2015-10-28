9d0f472d7d7ac67a6e90806238ce933af1e0ad07
Kelvin N <kelvin@kelvinism.com>
2015-10-29 11:23:22 +1300

    try/except around ping

--- app.py
+++ app.py
@@ -11,9 +11,15 @@ STATED_APIKEY = os.getenv('STATED_APIKEY', 'example@example.com')
 
 while True:
     sleep(1)
-    my_ip = os.getenv('MY_IP', '8.8.8.8')
-    p = Ping(my_ip, 2000, 55)
-    ms = p.run()
-    params = {"u":STATED_APIKEY, "stat":"ping", "value":str(ms)}
-    r = requests.get("https://stated.io/api", params=params )
-    print(str(ms) + "ms" + ": " + str(r.status_code))
+    try:
+        my_ip = os.getenv('MY_IP', '8.8.8.8')
+        p = Ping(my_ip, 2000, 55)
+        ms = p.run()
+    except:
+        print("Some exception")
+    if ms:
+        params = {"u":STATED_APIKEY, "stat":"ping", "value":str(ms)}
+        r = requests.get("https://stated.io/api", params=params )
+        print(str(ms) + "ms" + ": " + str(r.status_code))
+    else:
+        print("Ping failure")
