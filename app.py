43ab18e1881f34feee7eb5463350d17a381829ca
Kelvin N <kelvin@kelvinism.com>
2015-10-28 17:01:18 +1300

    Changing to not use influxdb

--- app.py
+++ app.py
@@ -2,36 +2,18 @@
 # coding: utf-8
 
 import os
+import requests
 from datetime import datetime
-from influxdb import InfluxDBClient
 from ping import Ping
 from time import sleep
 
-HOSTNAME = os.getenv('INFLUXDB_HOST', 'localhost')
-INFLUXDB_USERNAME = os.getenv('INFLUXDB_USERNAME', 'root')
-INFLUXDB_PASSWORD = os.getenv('INFLUXDB_PASSWORD', 'root')
-INFLUXDB_DATABASE = os.getenv('INFLUXDB_DATABASE', 'db1')
-MY_IP = os.getenv('MY_IP', '127.0.0.1')
-
-client = InfluxDBClient(HOSTNAME, 8086, INFLUXDB_USERNAME, INFLUXDB_PASSWORD, INFLUXDB_DATABASE)
+MY_IP = os.getenv('MY_IP', '8.8.8.8')
+STATED_APIKEY = os.getenv('STATED_APIKEY', 'example@example.com')
 
 while True:
     sleep(1)
     p = Ping(MY_IP, 2000, 55)
     ms = p.run()
-    json_body = [
-        {
-            "measurement": "ping",
-            "tags": {
-                "host": "server01",
-                "region": "us-east"
-            },
-            "time": str(datetime.now().isoformat()),
-            "fields": {
-                "value": float(ms)
-            }
-        }
-    ]
-    client.write_points(json_body)
-    result = client.query('select value from ping;')
-    print("Sent")
+    params = {"u":STATED_APIKEY, "stat":"ping", "value":str(ms)}
+    r = requests.get("https://stated.io/api", params=params )
+    print(str(ms) + "ms" + ": " + str(r.status_code))
