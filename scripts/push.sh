dfab83d1e724b721c1fd2f7483e8d74a02652832
Kelvin Nicholson <kelvin@kelvinism.com>
2019-05-24 20:59:15 +1000

    Remove Email part of login as no longer need

--- scripts/push.sh
+++ scripts/push.sh
@@ -3,6 +3,6 @@
 # Abort the script if any command fails
 set -e
 
-docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+docker login -u $DOCKER_USER -p $DOCKER_PASS
 docker tag ttl-im-docker zephell/ttl-im-docker:1.1.$SEMAPHORE_BUILD_NUMBER
 docker push zephell/ttl-im-docker:1.1.$SEMAPHORE_BUILD_NUMBER
