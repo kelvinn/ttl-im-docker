dac3b613cde09b9f0835e936e776e577d6dec2d5
Kelvin Nicholson <kelvin@kelvinism.com>
2019-05-24 21:43:47 +1000

    Typo

--- scripts/push.sh
+++ scripts/push.sh
@@ -4,5 +4,5 @@
 set -e
 
 docker login -u $DOCKER_USER -p $DOCKER_PASS
-docker tag ttl-im-docker zephell/ttl-im-docker:1.1.$TRAVIS_BUILD_DIR
-docker push zephell/ttl-im-docker:1.1.$TRAVIS_BUILD_DIR
+docker tag ttl-im-docker zephell/ttl-im-docker:1.1.$TRAVIS_BUILD_NUMBER
+docker push zephell/ttl-im-docker:1.1.$TRAVIS_BUILD_NUMBER
